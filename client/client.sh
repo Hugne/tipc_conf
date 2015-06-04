1ba1419fabacc8579f44a2b2f3baef77a9a8d842
Erik Hugne <erik.hugne@ericsson.com>
2015-06-04 13:46:25 +0200

    updated client
    Signed-off-by: Erik Hugne <erik.hugne@ericsson.com>

--- client/client.sh
+++ client/client.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-source ./lib/json.sh
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "$DIR/lib/json.sh"
 set +e
 
 SERV_NAME='TipcConfigService'
 SERV_TYPE='_http._tcp'
 CFG_REQ='/?request_config'
 
-function resolve {
+resolve () {
 	#lolers way of resolving an mdns service
 	res=`avahi-browse -vrptk $SERV_TYPE`
 	if [ $? -ne 0 ]; then
@@ -17,7 +18,7 @@ function resolve {
 	URL="http://$srv"
 }
 
-function get_config {
+get_config () {
 	res=`curl -s $URL$CFG_REQ`
 	if [ $? -ne 0 ]; then
 		echo "curl $URL$CFG_REQ failed" >&2
@@ -27,7 +28,20 @@ function get_config {
 		CONFIG=`json <<< $res`
 }
 
-function configure_address {
+get_bearer_param () {
+	idx=$1
+	param=$2
+	for row in $CONFIG
+	do
+		IFS=' ' read -a param <<< "$row"
+		if [ ${param[0]} == "/bearers/"$1"/$2" ]; then
+			echo ${param[2]}
+			return
+		fi
+	done
+}
+
+configure_address () {
 	#Get the TIPC address and assign it, there can be only one
 	for row in $CONFIG
 	do
@@ -36,11 +50,40 @@ function configure_address {
 			addr=${param[2]}
 		fi
 	done
-	echo "tipc node address set $addr"
+	echo "tipc node set addr $addr"
 }
 
-function configure_bearers {
-	echo "TODO"
+enable_eth () {
+	echo "Configure Ethernet media $1"
+	idx=$1
+	interface=$(get_bearer_param $idx "interface")
+	`tipc bearer enable media eth device $interface`
+}
+enable_udp () {
+	echo "Configure UDP media $1"
+	idx=$1
+	ip=$(get_bearer_param $idx "localip")
+	name=$(get_bearer_param $idx "name")
+	`tipc bearer enable media udp name $name localip $ip`
+}
+
+
+configure_bearers () {
+	i=0
+	for row in $CONFIG
+	do
+		IFS=' ' read -a param <<< "$row"
+		if [ ${param[0]} == "/bearers/"$i"/media" ]; then
+			if [ ${param[2]} == "eth" ];then
+				enable_eth $i
+				((i+=1))
+
+			elif [ ${param[2]} == "udp" ];then
+				enable_udp $i
+				((i+=1))
+			fi
+		fi
+	done
 
 }
 
